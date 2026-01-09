8f995094c0fd5ae2f1f8cd3f1608e51ef08120ba
Justyn Bell <Justyn.Bell@L3Harris.com>
2026-01-09 13:55:04 -0800

    More work to get FreeRTOS working.

--- app/src/main.c
+++ app/src/main.c
@@ -89,10 +89,10 @@ void vApplicationMallocFailedHook( void )
     for(;;);
 }
 
-void vConfigureTickInterrupt( void )
+void vConfigureTickInterrupt(void)
 {
     BaseType_t xStatus;
-    extern void FreeRTOS_Tick_Handler( void );
+    extern void FreeRTOS_Tick_Handler(void);
     XScuTimer_Config *pxTimerConfig;
     XScuGic_Config *pxGICConfig;
     const uint8_t ucRisingEdge = 3;
@@ -103,53 +103,89 @@ void vConfigureTickInterrupt( void )
 
 	/* Ensure XScuGic_CfgInitialize() has been called.  In this demo it has
 	already been called from prvSetupHardware() in main(). */
-	pxGICConfig = XScuGic_LookupConfig( XPAR_SCUGIC_SINGLE_DEVICE_ID );
-	xStatus = XScuGic_CfgInitialize( &xInterruptController, pxGICConfig, pxGICConfig->CpuBaseAddress );
-	configASSERT( xStatus == XST_SUCCESS );
-	( void ) xStatus; /* Remove compiler warning if configASSERT() is not defined. */
+	pxGICConfig = XScuGic_LookupConfig(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+	xStatus = XScuGic_CfgInitialize(&xInterruptController, pxGICConfig, pxGICConfig->CpuBaseAddress);
+	configASSERT(xStatus == XST_SUCCESS);
+	(void)xStatus; /* Remove compiler warning if configASSERT() is not defined. */
 
 	/* The priority must be the lowest possible. */
-	XScuGic_SetPriorityTriggerType( &xInterruptController, XPAR_SCUTIMER_INTR, portLOWEST_USABLE_INTERRUPT_PRIORITY << portPRIORITY_SHIFT, ucRisingEdge );
+	XScuGic_SetPriorityTriggerType(&xInterruptController, XPAR_SCUTIMER_INTR, portLOWEST_USABLE_INTERRUPT_PRIORITY << portPRIORITY_SHIFT, ucRisingEdge);
 
 	/* Install the FreeRTOS tick handler. */
-	xStatus = XScuGic_Connect( &xInterruptController, XPAR_SCUTIMER_INTR, (Xil_ExceptionHandler) FreeRTOS_Tick_Handler, ( void * ) &xTimer );
-	configASSERT( xStatus == XST_SUCCESS );
-	( void ) xStatus; /* Remove compiler warning if configASSERT() is not defined. */
+	xStatus = XScuGic_Connect(&xInterruptController, XPAR_SCUTIMER_INTR, (Xil_ExceptionHandler) FreeRTOS_Tick_Handler, (void *)&xTimer);
+	configASSERT(xStatus == XST_SUCCESS);
+	(void)xStatus; /* Remove compiler warning if configASSERT() is not defined. */
 
 	/* Initialise the timer. */
-	pxTimerConfig = XScuTimer_LookupConfig( XPAR_SCUTIMER_DEVICE_ID );
-	xStatus = XScuTimer_CfgInitialize( &xTimer, pxTimerConfig, pxTimerConfig->BaseAddr );
-	configASSERT( xStatus == XST_SUCCESS );
-	( void ) xStatus; /* Remove compiler warning if configASSERT() is not defined. */
+	pxTimerConfig = XScuTimer_LookupConfig(XPAR_SCUTIMER_DEVICE_ID);
+	xStatus = XScuTimer_CfgInitialize(&xTimer, pxTimerConfig, pxTimerConfig->BaseAddr);
+	configASSERT(xStatus == XST_SUCCESS);
+	(void)xStatus; /* Remove compiler warning if configASSERT() is not defined. */
 
 	/* Enable Auto reload mode. */
-	XScuTimer_EnableAutoReload( &xTimer );
+	XScuTimer_EnableAutoReload(&xTimer);
 
 	/* Ensure there is no prescale. */
-	XScuTimer_SetPrescaler( &xTimer, 0 );
+	XScuTimer_SetPrescaler(&xTimer, 0);
 
 	/* Load the timer counter register. */
-	XScuTimer_LoadTimer( &xTimer, (XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 2UL) / configTICK_RATE_HZ );
+	XScuTimer_LoadTimer(&xTimer, (XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 2UL) / configTICK_RATE_HZ);
 
 	/* Start the timer counter and then wait for it to timeout a number of
 	times. */
-	XScuTimer_Start( &xTimer );
+	XScuTimer_Start(&xTimer);
 
 	/* Enable the interrupt for the xTimer in the interrupt controller. */
-	XScuGic_Enable( &xInterruptController, XPAR_SCUTIMER_INTR );
+	XScuGic_Enable(&xInterruptController, XPAR_SCUTIMER_INTR);
 
 	/* Enable the interrupt in the xTimer itself. */
 	vClearTickInterrupt();
-	XScuTimer_EnableInterrupt( &xTimer );
+	XScuTimer_EnableInterrupt(&xTimer);
 }
 
-void vClearTickInterrupt( void )
+void vClearTickInterrupt(void)
 {
-	XScuTimer_ClearInterruptStatus( &xTimer );
+	XScuTimer_ClearInterruptStatus(&xTimer);
+}
+
+static void prvSetupHardware(void)
+{
+    BaseType_t xStatus;
+    XScuGic_Config *pxGICConfig;
+
+	/* Ensure no interrupts execute while the scheduler is in an inconsistent
+	state.  Interrupts are automatically enabled when the scheduler is
+	started. */
+	portDISABLE_INTERRUPTS();
+
+	/* Obtain the configuration of the GIC. */
+	pxGICConfig = XScuGic_LookupConfig(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+
+	/* Sanity check the FreeRTOSConfig.h settings are correct for the
+	hardware. */
+	configASSERT(pxGICConfig);
+	configASSERT(pxGICConfig->CpuBaseAddress == (configINTERRUPT_CONTROLLER_BASE_ADDRESS + configINTERRUPT_CONTROLLER_CPU_INTERFACE_OFFSET));
+	configASSERT(pxGICConfig->DistBaseAddress == configINTERRUPT_CONTROLLER_BASE_ADDRESS);
+
+	/* Install a default handler for each GIC interrupt. */
+	xStatus = XScuGic_CfgInitialize(&xInterruptController, pxGICConfig, pxGICConfig->CpuBaseAddress);
+	configASSERT(xStatus == XST_SUCCESS);
+	(void) xStatus; /* Remove compiler warning if configASSERT() is not defined. */
+
+	/* Initialise the LED port. */
+	//vParTestInitialise();
+
+	/* The Xilinx projects use a BSP that do not allow the start up code to be
+	altered easily.  Therefore the vector table used by FreeRTOS is defined in
+	FreeRTOS_asm_vectors.S, which is part of this project.  Switch to use the
+	FreeRTOS vector table. */
+	vPortInstallFreeRTOSVectorTable();
 }
 
 int main()
 {
+    prvSetupHardware();
+
     while (1) {
         printf("Hello, jworld\n");
         sleep(1);
