fa29e0e5362ce11fa8e12fc1f791ea6cae3b0f8d
Justyn Bell <Justyn.Bell@L3Harris.com>
2026-01-26 08:49:18 -0800

    Somehow this got out of sync.
    Need the FILESYSTEM stuff to compile the sd* FSBL stuff.

--- bsp/ps7_cortexa9_0/include/xparameters.h
+++ bsp/ps7_cortexa9_0/include/xparameters.h
@@ -584,6 +584,12 @@
 #define XPAR_XADCPS_0_BASEADDR 0xF8007100
 #define XPAR_XADCPS_0_HIGHADDR 0xF8007120
 
+/* Xilinx FAT File System Library (XilFFs) User Settings */
+#define FILE_SYSTEM_INTERFACE_SD
+#define FILE_SYSTEM_USE_MKFS
+#define FILE_SYSTEM_NUM_LOGIC_VOL 2
+#define FILE_SYSTEM_USE_STRFUNC 0
+#define FILE_SYSTEM_SET_FS_RPATH 0
 
 /******************************************************************/
 
