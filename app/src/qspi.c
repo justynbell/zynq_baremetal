c6479376b30ad15e1322dae13a3d5c5596974855
Justyn Bell <Justyn.Bell@L3Harris.com>
2026-02-04 11:19:30 -0800

    Cleanup, boot.bif fix, reads MAC address from QSPI chip's security registers (to get unique IP address if DHCP).
    Falls back to link local. That was an lwip option, not calling autoip_start() after dhcp_start().

---
+++ app/src/qspi.c
@@ -0,0 +1,65 @@
+#include <xqspips.h>
+
+#include "qspi.h"
+
+#define QSPI_DEVICE_ID XPAR_XQSPIPS_0_DEVICE_ID
+
+static XQspiPs QspiInstance;
+
+uint8_t qspi_init(void)
+{
+	XQspiPs_Config *QspiConfig;
+	int Status;
+
+	// Initialize the QSPI driver so that it's ready to use
+	QspiConfig = XQspiPs_LookupConfig(QSPI_DEVICE_ID);
+	if (NULL == QspiConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XQspiPs_CfgInitialize(&QspiInstance, QspiConfig,
+					QspiConfig->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	// Set Manual Chip select options and drive HOLD_B pin high.
+	XQspiPs_SetOptions(&QspiInstance, XQSPIPS_FORCE_SSELECT_OPTION |
+			XQSPIPS_HOLD_B_DRIVE_OPTION);
+
+	// Set the prescaler for QSPI clock
+	XQspiPs_SetClkPrescaler(&QspiInstance, XQSPIPS_CLK_PRESCALE_32);
+
+	// Assert the FLASH chip select.
+	XQspiPs_SetSlaveSelect(&QspiInstance);
+
+	return XST_SUCCESS;
+}
+
+uint8_t qspi_mac_read(uint8_t *mac)
+{
+	/* Size of the Read Security Registers command */
+	#define HEADER_SIZE 5
+	#define MAC_ADDR_SIZE 6
+
+    uint8_t readBuffer[HEADER_SIZE + MAC_ADDR_SIZE] = {0};
+	/* TODO: I don't know why we need a +1 here */
+    uint8_t writeBuffer[HEADER_SIZE + MAC_ADDR_SIZE + 1] = {0};
+    uint8_t status;
+
+	/* Location of MAC address in security registers */
+    uint32_t address = 0x001000;
+
+    writeBuffer[0] = 0x48;
+    writeBuffer[1] = (uint8_t)(address >> 16);
+    writeBuffer[2] = (uint8_t)(address >>  8);
+    writeBuffer[3] = (uint8_t)(address >>  0);
+    writeBuffer[4] = 0; /* Dummy byte per datasheet */
+
+    status = XQspiPs_PolledTransfer(&QspiInstance, writeBuffer, readBuffer, sizeof(writeBuffer));
+	if (status == XST_SUCCESS) {
+		memcpy(mac, readBuffer + HEADER_SIZE, MAC_ADDR_SIZE);
+	}
+
+    return status;
+}
---
+++ app/src/qspi.h
@@ -0,0 +1,7 @@
+#ifndef QSPI_H_
+#define QSPI_H_
+
+uint8_t qspi_init(void);
+uint8_t qspi_mac_read(uint8_t *mac);
+
+#endif /* QSPI_H_ */
